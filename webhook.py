89b216b3fa1cf891385e4a2f6de9fa4d2b31bd62
Noah Gallego <ngallego@csub.edu>
2025-09-21 23:30:41 -0700

    Edit Path?

--- webhook.py
+++ webhook.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from twilio.twiml import VoiceResponse
+from twilio.twiml.voice_response import VoiceResponse
 import os
 from dotenv import load_dotenv
 from agent import Agent
