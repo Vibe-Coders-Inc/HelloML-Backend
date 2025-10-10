ab036dfb694d9a0c6485c2eb37c427c74774f91c
Noah Gallego <ngallego@csub.edu>
2025-10-10 02:30:39 -0700

    API Changes

--- api/database.py
+++ api/database.py
@@ -3,16 +3,15 @@ from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
 
-# Load Environment Variables
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase_client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def supabase() -> Client: 
-    return supabase
+    return supabase_client
 
 if __name__ == "__main__":
     try:
