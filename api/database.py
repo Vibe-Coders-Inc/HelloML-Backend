5d5d77ef546a1a6ee45fe1185653cb651ba6f16f
Noah Gallego <ngallego@csub.edu>
2025-10-10 02:48:06 -0700

    Fix Supabase key variable name

--- api/database.py
+++ api/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_PUBLISHED_KEY")
 
 supabase_client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
